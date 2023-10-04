52ac83b123880a75378a63fa7f420ce39e6f9598
Angel <joangonzalezcr@ittepic.edu.mx>
2023-10-03 23:52:27 -0700

    Este commit ya tiene la gramática

---
+++ ElecatPlusIDE/gramatica/.antlr/SimpleBaseListener.java
@@ -0,0 +1,195 @@
+// Generated from c:/Users/angel/IdeaProjects/ElecatPlus/src/gramatica/Simple.g4 by ANTLR 4.13.1
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link SimpleListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class SimpleBaseListener implements SimpleListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterPrograma(SimpleParser.ProgramaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitPrograma(SimpleParser.ProgramaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSentencia(SimpleParser.SentenciaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSentencia(SimpleParser.SentenciaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDeclaracion(SimpleParser.DeclaracionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDeclaracion(SimpleParser.DeclaracionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAsignacion(SimpleParser.AsignacionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAsignacion(SimpleParser.AsignacionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCondicionalif(SimpleParser.CondicionalifContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCondicionalif(SimpleParser.CondicionalifContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCicloWhile(SimpleParser.CicloWhileContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCicloWhile(SimpleParser.CicloWhileContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAccion(SimpleParser.AccionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAccion(SimpleParser.AccionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEsperar(SimpleParser.EsperarContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEsperar(SimpleParser.EsperarContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterGirar(SimpleParser.GirarContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitGirar(SimpleParser.GirarContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEscribir(SimpleParser.EscribirContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEscribir(SimpleParser.EscribirContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTermino(SimpleParser.TerminoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTermino(SimpleParser.TerminoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpresion(SimpleParser.ExpresionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpresion(SimpleParser.ExpresionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOperacionBinaria(SimpleParser.OperacionBinariaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOperacionBinaria(SimpleParser.OperacionBinariaContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ ElecatPlusIDE/gramatica/.antlr/SimpleListener.java
@@ -0,0 +1,139 @@
+// Generated from c:/Users/angel/IdeaProjects/ElecatPlus/src/gramatica/Simple.g4 by ANTLR 4.13.1
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link SimpleParser}.
+ */
+public interface SimpleListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#programa}.
+	 * @param ctx the parse tree
+	 */
+	void enterPrograma(SimpleParser.ProgramaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#programa}.
+	 * @param ctx the parse tree
+	 */
+	void exitPrograma(SimpleParser.ProgramaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#sentencia}.
+	 * @param ctx the parse tree
+	 */
+	void enterSentencia(SimpleParser.SentenciaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#sentencia}.
+	 * @param ctx the parse tree
+	 */
+	void exitSentencia(SimpleParser.SentenciaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#declaracion}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeclaracion(SimpleParser.DeclaracionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#declaracion}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeclaracion(SimpleParser.DeclaracionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#asignacion}.
+	 * @param ctx the parse tree
+	 */
+	void enterAsignacion(SimpleParser.AsignacionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#asignacion}.
+	 * @param ctx the parse tree
+	 */
+	void exitAsignacion(SimpleParser.AsignacionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#condicionalif}.
+	 * @param ctx the parse tree
+	 */
+	void enterCondicionalif(SimpleParser.CondicionalifContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#condicionalif}.
+	 * @param ctx the parse tree
+	 */
+	void exitCondicionalif(SimpleParser.CondicionalifContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#cicloWhile}.
+	 * @param ctx the parse tree
+	 */
+	void enterCicloWhile(SimpleParser.CicloWhileContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#cicloWhile}.
+	 * @param ctx the parse tree
+	 */
+	void exitCicloWhile(SimpleParser.CicloWhileContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#accion}.
+	 * @param ctx the parse tree
+	 */
+	void enterAccion(SimpleParser.AccionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#accion}.
+	 * @param ctx the parse tree
+	 */
+	void exitAccion(SimpleParser.AccionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#esperar}.
+	 * @param ctx the parse tree
+	 */
+	void enterEsperar(SimpleParser.EsperarContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#esperar}.
+	 * @param ctx the parse tree
+	 */
+	void exitEsperar(SimpleParser.EsperarContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#girar}.
+	 * @param ctx the parse tree
+	 */
+	void enterGirar(SimpleParser.GirarContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#girar}.
+	 * @param ctx the parse tree
+	 */
+	void exitGirar(SimpleParser.GirarContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#escribir}.
+	 * @param ctx the parse tree
+	 */
+	void enterEscribir(SimpleParser.EscribirContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#escribir}.
+	 * @param ctx the parse tree
+	 */
+	void exitEscribir(SimpleParser.EscribirContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#termino}.
+	 * @param ctx the parse tree
+	 */
+	void enterTermino(SimpleParser.TerminoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#termino}.
+	 * @param ctx the parse tree
+	 */
+	void exitTermino(SimpleParser.TerminoContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#expresion}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpresion(SimpleParser.ExpresionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#expresion}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpresion(SimpleParser.ExpresionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SimpleParser#operacionBinaria}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperacionBinaria(SimpleParser.OperacionBinariaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SimpleParser#operacionBinaria}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperacionBinaria(SimpleParser.OperacionBinariaContext ctx);
+}
